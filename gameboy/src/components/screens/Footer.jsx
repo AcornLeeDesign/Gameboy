af6f0d7e531b68711de0a9073c626c62bec3692e
Aaron Lee <aarlee4824@gmail.com>
2025-12-08 17:51:32 -0800

    plain things

--- gameboy/src/components/screens/Footer.jsx
+++ gameboy/src/components/screens/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
   return (
     <div className="flex flex-col items-start text-left pr-16 pb-16 pt-12 pl-16">
-      <div className="text-[2.5em] p-8">Find me at:</div>
+      {/* <div className="text-[2.5em] p-8">Find me at:</div> */}
       <div className="text-[2.5rem] space-y-8">
-        <a href="https://www.instagram.com/aaron.so.u.know/?next=/" target="_blank" rel="noopener noreferrer" className="p-4 block">
+        {/* <a href="https://www.instagram.com/aaron.so.u.know/?next=/" target="_blank" rel="noopener noreferrer" className="p-4 block">
         [ instagram ]
         </a>
         <a href="https://x.com/acorn_lee_" target="_blank" rel="noopener noreferrer" className="p-4 block">
@@ -11,7 +11,8 @@ function Footer() {
         </a>
         <a href="https://www.linkedin.com/in/aaaronlee/" target="_blank" rel="noopener noreferrer" className="p-4 block">
         [ linkedin ]
-        </a>
+        </a> */}
+        <p>Imagining experiences to enhance curiosity and extend human imagination</p>
       </div>
     </div>
   );
